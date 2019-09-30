284811049df8ae60f15091175dfe9d41eb875856
Sam Whitney <sam@samwhitney.com>
2019-09-29 19:11:03 -0700

    add basic app

---
+++ flask_web/app.py
@@ -0,0 +1,15 @@
+# flask_web/app.py
+
+from flask import Flask, request, escape
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hey, we have a Flask app!'
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
